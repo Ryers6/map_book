c4596f15149c4c445003583f5480aecfac1cc63c
Ryers6 <166690739+Ryers6@users.noreply.github.com>
2024-04-29 10:58:01 +0200

    wip: Add a function to create user

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 from models.data import users
-from utils.crud import show_users
-
+from utils.crud import show_users, add_new_user, search_user,remove_user
+from
 
 
 if __name__ == "__main__":
     print("Witaj użytkowniku!")
     while True:
         print("Menu:")
+        print("0. Zakończ program")
         print("1. Wyświetl co u znajomych")
+        print("2. Dodaj użytkowkina")
+        print("3. Znajdź użytkownika")
+        print("4. Usuń użytkownika")
         menu_option: str = input("dokonaj wyboru:")
         if menu_option == "0":
             print("Program kończy pracę.")
             break
         if menu_option == "1":
             show_users(users)
+        if menu_option == "2":
+            add_new_user(users)
+        if menu_option == "3":
+            search_user(users)
+        if menu_option == "4":
+            remove_user(users)
--- utils/crud.py
+++ utils/crud.py
@@ -2,3 +2,10 @@ def show_users(user_list: list[dict]) -> None:
     for user in user_list:
         print(f"Twoj znajomy {user["name"]} opublikował: {user["posts"]}")
 
+def add_new_user(users: list)->None:
+    new_name: str = input("Enter your name: ")
+    new_surname: str = input("Enter your surname: ")
+    new_posts: str = int(input("Enter your posts: "))
+    new_user: dict = {"name": new_name, "surname": new_surname, "posts": new_posts}
+    print(new_user)
+    users.append(new_user)
